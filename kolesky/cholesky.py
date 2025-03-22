8a64925078296515b0f62c6b8f0eeb1ba87348a6
raymond-zeng <ruimin.zeng@gmail.com>
2025-03-22 00:35:15 -0400

    multiprocessing kolesky

--- kolesky/cholesky.py
+++ kolesky/cholesky.py
@@ -7,6 +7,7 @@ import scipy.linalg
 import scipy.sparse as sparse
 import sklearn.gaussian_process.kernels as kernels
 
+from multiprocessing import Pool, RawArray
 
 def __supernodes(sparsity, lengths, lamb):
     groups = []
@@ -25,7 +26,6 @@ def __supernodes(sparsity, lengths, lamb):
         for j in group[1:]:
             agg_sparsity[j] = np.empty(len(s) - positions[j], dtype=int)
     return groups, agg_sparsity
-
 
 def __cols(theta):
     return np.flip(np.linalg.cholesky(np.flip(theta))).T
@@ -47,9 +47,38 @@ def __aggregate_chol(points, kernel, sparsity, groups):
             indices[ptr[i] : ptr[i + 1]] = s[k:]
     return sparse.csc_matrix((data, indices, ptr), shape=(n, n))
 
+def init_shared(data, indices):
+    global shared_data, shared_indices
+    shared_data = data
+    shared_indices = indices
+
+def group_chol(points, kernel, sparsity, group, ptr):
+    s = sorted(sparsity[group[0]])
+    positions = {i: k for k, i in enumerate(s)}
+    L_group = __cols(kernel(points[s]))
+    for i in group:
+        k = positions[i]
+        e_k = np.zeros(len(s))
+        e_k[k] = 1
+        col = scipy.linalg.solve_triangular(L_group, e_k, lower=True, check_finite=False)
+        shared_data[ptr[i] : ptr[i + 1]] = col[k:]
+        shared_indices[ptr[i] : ptr[i + 1]] = s[k:]
+
+def parallel_aggregate_chol(points, kernel, sparsity, groups):
+    n = len(points)
+    ptr = np.cumsum([0] + [len(sparsity[i]) for i in range(n)])
+    data = RawArray('d', int(ptr[-1]))
+    indices = RawArray('i', int(ptr[-1]))
+    with Pool(initializer=init_shared, initargs=(data, indices)) as pool:
+        pool.starmap(group_chol, [(points, kernel, sparsity, group, ptr) for group in groups])
+    data_np = np.frombuffer(data, dtype=np.float64)
+    indices_np = np.frombuffer(indices, dtype=np.int32)
+    return sparse.csc_matrix((data_np, indices_np, ptr), shape=(n, n))
+
 def kl_cholesky(points, kernel, rho, lamb, initial = None, p = 1):
     indices, lengths = p_reverse_maximin(points, initial, p)
     ordered_points = points[indices]
     sparsity = sparsity_pattern(ordered_points, lengths, rho)
     groups, agg_sparsity = __supernodes(sparsity, lengths, lamb)
-    return __aggregate_chol(ordered_points, kernel, agg_sparsity, groups), indices
+    # return __aggregate_chol(ordered_points, kernel, agg_sparsity, groups), indices
+    return parallel_aggregate_chol(ordered_points, kernel, agg_sparsity, groups), indices
--- test.py
+++ test.py
@@ -3,7 +3,7 @@ import sklearn.gaussian_process.kernels as kernels
 import kolesky
 
 np.random.seed(0)
-n = 50
+n = 10
 points = np.zeros((n * n, 2))
 for i in range(n):
     for j in range(n):
